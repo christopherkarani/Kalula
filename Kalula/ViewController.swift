a1d67269063886f1d2043b86f6e11ab39ffa6d95
Chris Karani <chrisbkarani@gmail.com>
2017-12-19 22:29:07 +0300

    Initial Commit

---
+++ Kalula/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kalula
+//
+//  Created by Christopher Brandon Karani on 19/12/2017.
+//  Copyright © 2017 Christopher Brandon Karani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
