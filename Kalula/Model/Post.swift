921b40e9d0ac343f245aaaa8e3e5d12091c6e09d
xrisyz <chrisbkarani@gmail.com>
2018-03-30 22:37:52 +0300

    download images to userprofileVC

---
+++ Kalula/Model/Post.swift
@@ -0,0 +1,9 @@
+//
+//  Post.swift
+//  Kalula
+//
+//  Created by Chris Karani on 3/30/18.
+//  Copyright © 2018 Christopher Brandon Karani. All rights reserved.
+//
+
+import Foundation
---
+++ Kalula/Views/UserPostsCell.swift
@@ -0,0 +1,9 @@
+//
+//  UserPostsCell.swift
+//  Kalula
+//
+//  Created by Chris Karani on 3/30/18.
+//  Copyright © 2018 Christopher Brandon Karani. All rights reserved.
+//
+
+import Foundation
