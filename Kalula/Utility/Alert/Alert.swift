3d140eed3025e0ce7401129ce55eca987fe240e7
Chris Karani <christopher.karani@incentro.com>
2018-09-24 23:43:47 +0300

    remodeling Session Error, and used this time to create a new way of presenting errors in the app

---
+++ Kalula/Utility/Alert/Alert.swift
@@ -0,0 +1,21 @@
+//
+//  AlertDisplay.swift
+//  Kalula
+//
+//  Created by Chris Karani on 24/09/2018.
+//  Copyright © 2018 Christopher Brandon Karani. All rights reserved.
+//
+
+import UIKit.UIAlertController
+import UIKit.UIViewController
+
+protocol Alert {
+    var title: String { get }
+    var error: Error { get }
+}
+
+
+
+
+
+
---
+++ Kalula/Utility/UIViewController/UIViewController_Ext.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController_Ext.swift
+//  Kalula
+//
+//  Created by Chris Karani on 24/09/2018.
+//  Copyright © 2018 Christopher Brandon Karani. All rights reserved.
+//
+
+import Foundation
